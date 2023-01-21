36ffb4a9d09188b22c1c9685cd4e276ebb1d4816
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-22 00:41:07 +0200

    Update Phonebook.jsx

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -2,6 +2,12 @@ export const Phonebook = () => {
   return (
     <div className="Phonebook-container">
       <h1 className="Phonebook__title">Phonebook</h1>
+      <div className="Phonebook__form-container">
+        <button
+          type="submit"
+          className="Phonebook__form-submit-button"
+        ></button>
+      </div>
     </div>
   );
 };
