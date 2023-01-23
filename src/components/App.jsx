24fe547699e406df374020e3cdf0712b64839b98
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-23 20:51:04 +0200

    Destructured argument in ContactsList.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import React, { Component } from 'react';
 import { ContactsList } from './Contacts/ContactsList';
 import { Form } from './Form/Form';
@@ -5,16 +6,15 @@ import { Form } from './Form/Form';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
   };
 
-  formSubmitData = data => {
+  formSubmitData = (data, e) => {
+    e.preventDefault();
     console.log(data);
   };
 
@@ -23,7 +23,7 @@ export class App extends Component {
       <div className="Phonebook-container">
         <h1 className="Phonebook__title">Phonebook</h1>
 
-        <Form contacts={this.state.contacts} onSubmit={this.formSubmitData} />
+        <Form onSubmit={this.formSubmitData} />
 
         <ContactsList contacts={this.state.contacts} />
       </div>
