13ef093a2c84775bbeb3eeb8cbccc54d40200267
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-25 11:27:29 +0200

    Fix issue with inputs (value from state)

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -19,7 +19,7 @@ export class Form extends Component {
 
     this.props.onSubmit(this.state);
 
-    // this.reset();
+    this.reset();
   };
 
   reset = () => {
@@ -43,6 +43,7 @@ export class Form extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
+              value={this.state.name}
               onChange={this.handleChange}
             />
           </label>
@@ -55,6 +56,7 @@ export class Form extends Component {
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
+              value={this.state.number}
               onChange={this.handleChange}
             />
           </label>
