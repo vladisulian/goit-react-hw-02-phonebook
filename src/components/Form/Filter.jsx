a46695e24eddcb956430e59f9034ccefaa8c49ef
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-25 12:14:44 +0200

    Add more styles

--- src/components/Form/Filter.jsx
+++ src/components/Form/Filter.jsx
@@ -1,9 +1,17 @@
 import PropTypes from 'prop-types';
+import './Filter.css';
 export const FilterBar = ({ handleChange }) => {
   return (
     <>
       <h3>Find your contacts by name</h3>
-      <input type="text" name="filter" onChange={handleChange} />
+      <div className="filter-container">
+        <input
+          type="text"
+          name="filter"
+          className="filter__input"
+          onChange={handleChange}
+        />
+      </div>
     </>
   );
 };
